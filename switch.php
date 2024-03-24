5aced115570ec8522147cdc5e1042c8a569f4cf2
haru <haru@example.com>
2024-03-24 00:22:05 +0000

    for.php

--- switch.php
+++ switch.php
@@ -5,15 +5,15 @@ switch($i) {
     case 0:
         echo "i は 0です \n";
         break;
-        
+
     case 1:
         echo "i は 1です \n";
-        break;    
-        
-        
+        break;
+
+
     case 2:
         echo "i は 0と1 以外です \n";
-        break;    
+        break;
 }
 
 // PHP 8未満だと特に注意が必要なコード
@@ -22,13 +22,13 @@ switch($i) {
     case '2a':
         echo "i は '2a'です \n";
         break;
-        
+
     case '2':
         echo "i は '2'(string)です \n";
-        break;    
-        
+        break;
+
     case 2:
         echo "i は 2(int)です \n";
-        break;    
+        break;
 }
 
