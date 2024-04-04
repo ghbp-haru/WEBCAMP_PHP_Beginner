49a1bd5ec64b817f1d7b31d158e3c622fa332f99
haru <haru@example.com>
2024-04-04 21:52:51 +0000

    csv_write.phpを書いた

---
+++ csv_write.php
@@ -0,0 +1,17 @@
+<?php
+
+$write_date = [
+    [1, 2, 3,],
+    ["aaa", "bbb", "ccc"],
+    [",aa", "b \" b", "c \n c"],
+    ];
+
+
+    $file_name = __DIR__ . "/data.csv";
+
+    $file_obj = new SplFileObject($file_name,"w");
+
+    foreach($write_data as $datum) {
+
+        $file_obj->fputcsv($datum);
+    }
